826a85550d0be65a49772b0896c5b3bd9a3f783d
Michael Rayson <mrayson5129@gmail.com>
2021-10-02 18:05:51 -0400

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './components/error404/error404.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'homepage', component: HomepageComponent },
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+  { path: '**', component: Error404Component }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    anchorScrolling: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { Error404Component } from './components/error404/error404.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    Error404Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
