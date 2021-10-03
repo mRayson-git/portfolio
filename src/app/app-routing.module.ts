70482ac36e2c9a1bf12fa9d45bb2a4da52219957
Michael Rayson <mrayson5129@gmail.com>
2021-10-03 19:00:22 -0400

    added resume

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404Component } from './components/error404/error404.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
 const routes: Routes = [
   { path: 'homepage', component: HomepageComponent },
+  { path: 'resume', component: ResumeComponent },
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
   { path: '**', component: Error404Component }
 ];
